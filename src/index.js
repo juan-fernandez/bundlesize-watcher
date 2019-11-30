81fdaff2d4372cca143d617800d6f61e0abda8bb
Juan Antonio Fernández de Alba <juan@undefinedlabs.com>
2019-11-30 18:43:02 +0100

    new approach

--- src/index.js
+++ src/index.js
@@ -4,40 +4,19 @@ const path = require('path')
 const glob = require('glob')
 const fs = require('fs')
 const loadJsonFile = require('load-json-file')
-const firebase = require('firebase')
-require('firebase/firestore')
-
-function initializeFirebase() {
-  const firebaseApiKey = core.getInput('firebase_apikey')
-  const firebaseConfig = {
-    apiKey: firebaseApiKey,
-    authDomain: 'bundlewatcher-github.firebaseapp.com',
-    databaseURL: 'https://bundlewatcher-github.firebaseio.com',
-    projectId: 'bundlewatcher-github',
-    storageBucket: 'bundlewatcher-github.appspot.com',
-    messagingSenderId: '78156893526',
-    appId: '1:78156893526:web:d487cfe28faffa311e5af7',
-    measurementId: 'G-LTBCB0ZSPB',
-  }
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig)
-  firebase.analytics()
-  return firebase.database()
-}
 
 async function run() {
-  const firebaseDatabase = initializeFirebase()
   await exec.exec('yarn build', null, {
     ignoreReturnCode: true,
   })
   const { GITHUB_SHA, GITHUB_REF, GITHUB_EVENT_NAME } = process.env
   const configFile = core.getInput('configFile') || 'bundlewatcher.json'
 
-  let fileSetting, maxSizeSetting
+  let inputFileSetting, inputMaxSizeSetting
   try {
     const [readConfigFile] = await loadJsonFile(`./${configFile}`)
-    fileSetting = readConfigFile.file
-    maxSizeSetting = readConfigFile.maxSize
+    inputFileSetting = readConfigFile.file
+    inputMaxSizeSetting = readConfigFile.maxSize
   } catch (error) {
     core.setFailed(error.message)
     throw Error('Config file not found')
@@ -45,7 +24,7 @@ async function run() {
 
   try {
     const [mainFile] = await new Promise(resolve => {
-      glob(file, null, (err, files) => {
+      glob(inputFileSetting, null, (err, files) => {
         resolve(files)
       })
     })
@@ -53,10 +32,26 @@ async function run() {
       throw Error('Main file not found')
     }
     const { size: sizeInBytes } = fs.statSync(mainFile)
-    const sizeInKiloBytes = sizeInBytes / 1000
-    database.ref(`bundlesize/${GITHUB_SHA}`).set({
-      size: sizeInBytes,
+
+    console.log('size branch', sizeInBytes)
+
+    await exec.exec('git checkout master')
+
+    await exec.exec('yarn build', null, {
+      ignoreReturnCode: true,
     })
+
+    const [mainFileMaster] = await new Promise(resolve => {
+      glob(inputFileSetting, null, (err, files) => {
+        resolve(files)
+      })
+    })
+
+    const { size: sizeInBytesMaster } = fs.statSync(mainFile)
+
+    console.log('size branch master', sizeInBytesMaster)
+
+    console.log('difference', sizeInBytes - sizeInBytesMaster)
 
     core.setFailed('error to rerun')
   } catch (error) {
