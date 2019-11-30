03243e45ee4b9a94effc383d4220f8f49386e26f
Juan Antonio Fernández de Alba <juan@undefinedlabs.com>
2019-11-30 13:26:24 +0100

    add bundle size

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const core = require('@actions/core')
+const exec = require('@actions/exec')
+const path = require('path')
+const fs = require('fs')
+
+const MAIN_FILE_PATH = './build/static/js/main.*.chunk.js'
+
+async function run() {
+  await exec.exec('yarn build', null, {
+    ignoreReturnCode: true,
+  })
+  const { size: sizeInBytes } = await fs.stat(MAIN_FILE_PATH)
+  const sizeInKiloBytes = sizeInBytes / 1000
+  console.log(`Main bundle size: ${sizeInKiloBytes}KB`)
+}
+
+run()
